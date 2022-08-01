166be32b23ef638e782826913e42356f78c5dd58
Michael Jurke <m.jurke@gmx.de>
2022-08-01 17:09:35 +0200

    Only set default br on partner, if the partner is newly created

--- business_relationships/models/res_users.py
+++ business_relationships/models/res_users.py
@@ -6,14 +6,18 @@ class Users(models.Model):
     _inherit = "res.users"
 
     @api.model_create_multi
-    def create(self, vals_list):
+    def create(self, values_list):
         # we cannot know all custom modules, installation order and all group
         # configurations.
         # Furthermore, not using model_create_multi notably slows down bulk create.
         # So, we fix the business relationship of the associated partner
         # after user creation.
-        users = super().create(vals_list)
-        for user in users:
+        users = super().create(values_list)
+        for user, values in zip(users, values_list):
+            # the partner used to exist and we keep his business relationship
+            if values.get("partner_id", False):
+                continue
+
             partner = user.partner_id
             if partner:
                 if user.has_group("base.group_user"):
