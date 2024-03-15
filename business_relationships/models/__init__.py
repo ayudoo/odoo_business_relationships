bf491b41e00d8c3efff75bf85de5b6c25fa17149
Michael Jurke <m.jurke@gmx.de>
2024-03-15 18:20:32 +0100

    Streamline file names

--- business_relationships/models/__init__.py
+++ business_relationships/models/__init__.py
@@ -2,9 +2,9 @@ from . import (
     account_invoice,
     business_relationship,
     fiscal_position,
-    partner,
     product_pricelist,
     res_config_settings,
+    res_partner,
     res_users,
     sale_order,
 )
